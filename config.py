fedeb6581655ab9fe278f0fe84fb679f1e38e853
0utsid3 <128305783+0utsid3@users.noreply.github.com>
2024-05-07 10:33:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 settings = {
-    'token': "MTIzNDQ4NzA4NDcwNjY5NzI3Ng.GxBqT4.NLrJJa4H30XdrLzOptT9A5_8ZLjsIFZ2FNxIlk",
-    'bot': "Xtazy",
-    'id': 1234487084706697276,
+    'token': "token",
+    'bot': "name bot",
+    'id': id,
     'prefix': "!",
-    'VK_ACCESS_TOKEN': 'tlnqBbvT5Uh3VWe8EXtb',
-    'log_channel_id': 1234595037459910747,
-    'seconds': ""
+    'VK_ACCESS_TOKEN': 'vk token',
+    'log_channel_id': id,
+    
 }
 # Словарь для хранения информации о мутах пользователей {user_id: (время начала мута, продолжительность мута)}
 muted_users = {
