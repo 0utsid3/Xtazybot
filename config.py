315763af7bada1fe7453a0cbff95c07ed0559fe2
0utsid3 <128305783+0utsid3@users.noreply.github.com>
2024-05-07 10:29:41 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+settings = {
+    'token': "MTIzNDQ4NzA4NDcwNjY5NzI3Ng.GxBqT4.NLrJJa4H30XdrLzOptT9A5_8ZLjsIFZ2FNxIlk",
+    'bot': "Xtazy",
+    'id': 1234487084706697276,
+    'prefix': "!",
+    'VK_ACCESS_TOKEN': 'tlnqBbvT5Uh3VWe8EXtb',
+    'log_channel_id': 1234595037459910747,
+    'seconds': ""
+}
+# Словарь для хранения информации о мутах пользователей {user_id: (время начала мута, продолжительность мута)}
+muted_users = {
+
+}
